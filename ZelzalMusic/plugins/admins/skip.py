17ac11c6da5b5091b73b3af6157451f5bc8c819b
FF30F <158225203+FF30F@users.noreply.github.com>
2024-02-23 16:30:05 +0300

    Update skip.py

--- ZelzalMusic/plugins/admins/skip.py
+++ ZelzalMusic/plugins/admins/skip.py
@@ -25,7 +25,6 @@ async def skip(cli, message: Message, _, chat_id):
         loop = await get_loop(chat_id)
         if loop != 0:
             return await message.reply_text(_["admin_8"])
-        user_mention = message.from_user.mention if message.from_user else "المشـرف"
         state = message.text.split(None, 1)[1].strip()
         if state.isnumeric():
             state = int(state)
@@ -47,7 +46,7 @@ async def skip(cli, message: Message, _, chat_id):
                                 try:
                                     await message.reply_text(
                                         text=_["admin_6"].format(
-                                            user_mention,
+                                            message.from_user.mention,
                                             message.chat.title,
                                         ),
                                         reply_markup=close_markup(_),
@@ -74,7 +73,7 @@ async def skip(cli, message: Message, _, chat_id):
             if not check:
                 await message.reply_text(
                     text=_["admin_6"].format(
-                        user_mention, message.chat.title
+                        message.from_user.mention, message.chat.title
                     ),
                     reply_markup=close_markup(_),
                 )
@@ -86,7 +85,7 @@ async def skip(cli, message: Message, _, chat_id):
             try:
                 await message.reply_text(
                     text=_["admin_6"].format(
-                        user_mention, message.chat.title
+                        message.from_user.mention, message.chat.title
                     ),
                     reply_markup=close_markup(_),
                 )
@@ -95,7 +94,7 @@ async def skip(cli, message: Message, _, chat_id):
                 return
     queued = check[0]["file"]
     title = (check[0]["title"]).title()
-    user = check[0]["by"] if check[0] else "المشـرف"
+    user = check[0]["by"]
     streamtype = check[0]["streamtype"]
     videoid = check[0]["vidid"]
     status = True if str(streamtype) == "video" else None
